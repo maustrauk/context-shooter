68987bc4bf126952959241debca39809e49d56f7
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-01 12:15:06 -0400

    adding Canvas to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 
 class App extends Component {
   render() {
     return (
-      <div>Hi</div>
+      <Canvas />
     );
   }
 }
+
+App.propTypes = {
+  message: PropTypes.string,
+};
 
 export default App;
