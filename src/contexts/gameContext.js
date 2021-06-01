7da36d7e09a252ceb72bce2164cae79beb9fb302
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-01 12:53:32 -0400

    Adding angleContext

---
+++ src/contexts/gameContext.js
@@ -0,0 +1,25 @@
+import React, {useState, createContext } from 'react';
+
+
+export const AngleContext = createContext({
+    angle: '',
+    setAngle: () => {},
+  });
+
+  export const AngleContextProvider = props => {
+    const setAngle = angle => {
+      setState({ ...state, angle: angle });
+    };
+  
+  
+    const initState = {
+        setAngle: setAngle,
+      angle: '45',
+    };
+  
+    const [state, setState] = useState(initState);
+  
+    return (
+      <AngleContext.Provider value={state}>{props.children}</AngleContext.Provider>
+    );
+  };
