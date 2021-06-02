817225e30a6f89e366f3bd80d7582d1b592d19c9
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-01 22:18:33 -0400

    Removing unused constant

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Canvas from './components/Canvas';
 
 
 class App extends Component  {
-
+  
   componentDidMount() {
     window.onresize = () => {
       const cnv = document.getElementById('shooter-game-canvas');
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,3 @@ export const skyAndGroundWidth = 5000;
 export const gameWidth = 800;
 
 export const gameHeight = 1200;
-
-export const cannonPipePos = {
-    x: window.innerWidth / 2,
-    y: window.innerHeight - 160,
-  }
