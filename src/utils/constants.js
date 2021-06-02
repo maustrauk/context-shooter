b29022f770e06c3a74a5bee7ff8a7fe6e4ad9c4a
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-01 21:46:09 -0400

    Adding responsive constans

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const skyAndGroundWidth = 5000;
+
 export const gameWidth = 800;
+
+export const gameHeight = 1200;
+
+export const cannonPipePos = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight - 160,
+  }
