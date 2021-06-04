ac416b7b1a81985fa1f8e13fa1a209b8985bab4b
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-04 16:35:11 -0400

    Adding auth0 to the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
 
+Auth0.configure({
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component  {
   
   componentDidMount() {
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      console.log("Logined - ",auth);
+    });
+
     window.onresize = () => {
       const cnv = document.getElementById('shooter-game-canvas');
       cnv.style.width = `${window.innerWidth}px`;
       cnv.style.height = `${window.innerHeight}px`;
     };
     window.onresize();
+
+    
   }
 
   render() {
