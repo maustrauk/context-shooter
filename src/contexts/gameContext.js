8059d90fdd0bdae17b91fb17b6755883195c455f
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-04 12:20:07 -0400

    Changing Contrext API states

--- src/contexts/gameContext.js
+++ src/contexts/gameContext.js
@@ -1,126 +1,84 @@
-import { bool, number } from 'prop-types';
 import React, { useState, createContext } from 'react';
 
 import { initialFlyingObjects, initialCreatedAt } from '../utils/constants';
 
+//Initials states
+
+const AngleInitState = {
+  angle: 0,
+  mousePosition: {
+    x: 0,
+    y: 0,
+  },
+  cannonBaseCenter: {
+    x: 1200,
+    y: 800,
+  },
+};
+
+const GameplayInitState = {
+  kills: 0,
+  lives: 0,
+  startGame: false,
+};
+
+const FlyingObjectInitState = {
+  flyingObjects: initialFlyingObjects,
+  lastObjectCreatedAt: initialCreatedAt,
+};
+
+
+
 //GameplayContext
 
-export const GameplayContext = createContext({
-  startGame: bool,
-  kills: number,
-  lives: number,
-  setStartGame: () => {/*Some function*/},
-  setKills: () => {/*Some function*/},
-  setLives: () => {/*Some function*/},
-});
+export const GameplayContext = createContext();
 
 export const GameplayContextProvider = props => {
 
-  const setStartGame = (startGame) => {
-      setState({ ...state, startGame: startGame});
+  const [state, setState] = useState(GameplayInitState);
+
+  const setGameplayContextState = (kills, lives, startGame) => {
+      setState({ ...state, kills: kills, lives: lives, startGame: startGame});
     };
 
-  const setKills = (kills) => {
-    setState({ ...state, kills: kills});
-  };
-
-  const setLives = (lives) => {
-    setState({ ...state, lives: lives});
-  };
-
-  const initState = {
-    setStartGame: setStartGame,
-    setKills: setKills,
-    setLives: setLives,
-    kills: 0,
-    lives: 0,
-    startGame: false,
-  };
-
-  const [state, setState] = useState(initState);
 
   return (
-    <GameplayContext.Provider value={state}>{props.children}</GameplayContext.Provider>
+    <GameplayContext.Provider value={{...state, setGameplayContextState}}>{props.children}</GameplayContext.Provider>
   );
 };
 
 //FlyingObjectContext
 
-export const FlyingObjectContext = createContext({
-  flyingObjects: [],
-  lastObjectCreatedAt: number,
-  setFlyingObjects: () => {/*Some function*/},
-  setLastObjectCreatedAt: () => {/*Some function*/},
-});
+export const FlyingObjectContext = createContext();
 
 export const FlyingObjectContextProvider = props => {
 
-  const setFlyingObjects = (flyingObjects) => {
-    setState({ ...state, flyingObjects: flyingObjects});
-  };
+  const [state, setState] = useState(FlyingObjectInitState);
 
-  const setLastObjectCreatedAt = (lastObjectCreatedAt) => {
-    setState({ ...state, lastObjectCreatedAt: lastObjectCreatedAt});
+  const setFlyingObjectContext = (flyingObjects, lastObjectCreatedAt) => {
+    setState({ ...state, flyingObjects: flyingObjects, lastObjectCreatedAt: lastObjectCreatedAt});
   };
-
-  const initState = {
-    setFlyingObjects: setFlyingObjects,
-    setLastObjectCreatedAt: setLastObjectCreatedAt,
-    flyingObjects: initialFlyingObjects,
-    lastObjectCreatedAt: initialCreatedAt,
-  };
-
-  const [state, setState] = useState(initState);
 
   return (
-    <FlyingObjectContext.Provider value={state}>{props.children}</FlyingObjectContext.Provider>
+    <FlyingObjectContext.Provider value={{...state, setFlyingObjectContext}}>{props.children}</FlyingObjectContext.Provider>
   );
 };
 
 //AngleContext
 
-export const AngleContext = createContext({
-    angle: number,
-    mousePosition: {},
-    cannonBaseCenter: {},
-    setMousePosition: () => {/*Some function*/},
-    setCannonBaseCenter: () => {/*Some function*/},
-    setAngle: () => {/*Some function*/},
-  });
 
-  export const AngleContextProvider = props => {
 
-    const setMousePosition = (mousePosition) => {
-        setState({ ...state, mousePosition: mousePosition});
+export const AngleContext = createContext();
+
+export const AngleContextProvider = props => {
+
+  const [state, setState] = useState(AngleInitState);
+
+    const setAngelContextState = (angle, mousePosition, cannonBaseCenter) => {
+        setState({ ...state, angle: angle, mousePosition: mousePosition, cannonBaseCenter: cannonBaseCenter});
       };
 
-    const setCannonBaseCenter = cannonBaseCenter => {
-      setState({ ...state, cannonBaseCenter: cannonBaseCenter});
-    };
-
-    const setAngle = angle => {
-      setState({ ...state, angle: angle});
-    };
-  
-  
-    const initState = {
-        setMousePosition: setMousePosition,
-        setCannonBaseCenter: setCannonBaseCenter,
-        setAngle: setAngle,
-        angle: 0,
-        mousePosition: {
-            x: 0,
-            y: 0,
-        },
-        cannonBaseCenter: {
-          x: 1200,
-          y: 800,
-        },
-    };
-  
-    const [state, setState] = useState(initState);
-  
     return (
-      <AngleContext.Provider value={state}>{props.children}</AngleContext.Provider>
+      <AngleContext.Provider value={{...state, setAngelContextState}}>{props.children}</AngleContext.Provider>
     );
   };
