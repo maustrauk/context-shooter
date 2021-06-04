fbeb7bb3df739af320bab491365cfee1cd6d3512
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-04 17:00:47 -0400

    Adding Leaderboard

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -16,9 +16,22 @@ import FlyingObject from './FlyingObject';
 import Heart from './Heart';
 import StartGame from './StartGame';
 import Title from './Title';
-import Login from './Login';
+import Leaderboard from './Leaderboard';
 
 const Canvas = (props) => {
+
+  //Mock data
+  const leaderboard = [
+    { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/profile_image', },
+    { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/profile_image', },
+    { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/profile_image', },
+    { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/profile_image', },
+    { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/profile_image', },
+    { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/profile_image', },
+    { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/profile_image', },
+    { id: 'h8', maxScore: 146, name: 'Sebastian Peyrott', picture: 'https://twitter.com/speyrott/profile_image', },
+  ];
+
     const angleContext = useContext(AngleContext);
     const gameplayContext = useContext(GameplayContext);
     const flyingObjectContext = useContext(FlyingObjectContext);
@@ -80,7 +93,7 @@ const Canvas = (props) => {
             <g>
               <StartGame onClick={startButtonHandler} />
               <Title/>
-              <Login authenticate={signIn} />
+              <Leaderboard currentPlayer={leaderboard[6]} authenticate={signIn} leaderboard={leaderboard} />
             </g>
           }
       </svg>
