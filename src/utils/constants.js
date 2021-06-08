c7939cf533885d27f488f612a4c96e956c3f221d
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-07 20:14:27 -0400

    Remoiving redundant constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,28 +4,6 @@ export const gameWidth = 800;
 
 export const gameHeight = 1200;
 
-
-//flyingObjects
-
-export const initialCreatedAt = (new Date()).getTime();
-
-export const initialFlyingObjects = [
-    {
-        position: {
-            x: -150,
-            y: -600
-        },
-        createdAt: initialCreatedAt
-    },
-    {
-        position: {
-            x: 150,
-            y: -600
-        },
-        createdAt: initialCreatedAt
-    }
-];
-
 export const createInterval = 1000;
 
 export const maxFlyingObjects = 4;
