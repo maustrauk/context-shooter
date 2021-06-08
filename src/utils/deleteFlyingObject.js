1ac10112019a1169a6c6bc1068a8f1459cd8d7ad
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-07 20:12:00 -0400

    Removing reedundant code:

--- src/utils/deleteFlyingObject.js
+++
@@ -1,8 +0,0 @@
-const deleteFlyingObject = (flyingObjectsContext) => {
-    const now = (new Date()).getTime();
-    const lastObjectCreatedAt = flyingObjectsContext.lastObjectCreatedAt;
-    const filteredObjects = [...flyingObjectsContext.flyingObjects].filter(object => ((now - object.createdAt) < 3000));
-    return flyingObjectsContext.setFlyingObjectContext(filteredObjects, lastObjectCreatedAt);
-};
-
-export default deleteFlyingObject;
